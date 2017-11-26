8f4f2fec01a8e1f422a3debfdf31be43b41ecd51
Michal <pulka.michal@gmail.com>
2017-11-26 20:14:57 +0100

    Add comments scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
 
 var name = prompt('Enter your name');
+// I why not write please enter your name
 alert('Hello, ' + name);
+/*
+  alert('This comment will not be display');
+*/
 console.log('Hello, ' + name);
+// console.log (I don't think this is necessary)
